9aca8a0e83b1b01d643869381cfbf7ad9c75bffd
Isma <Isma@PC>
2021-02-13 20:43:22 +0100

    2º Commit - añade Javadoc

--- deposito/cuentas/CCuenta.java
+++ deposito/cuentas/CCuenta.java
@@ -1,30 +1,67 @@
 package cuentas;
+
+/**
+ * Clase que contiene los métodos necesarios para la gestión de una cuenta bancaria
+ * 
+ * @author IsmaelMorenoAlfaro
+ * @version 2.0
+ */
 public class CCuenta {
 
+	/**
+	 * Variables de clase de CCuenta
+	 */
 	private String nombre;
 	private String cuenta;
 	private double saldo;
 	private double tipoInteres;
 
+	/**
+	 * Constructor por defecto
+	 */
 	public CCuenta() {
 	}
 
+	/**
+	 * Constructor que inicializa los atributos de clase de CCuenta
+	 * @param nom - Nombre del cliente
+	 * @param cue - Número de cuenta
+	 * @param sal - Saldo de la cuenta
+	 * @param tipo - Tipo de interés
+	 */
 	public CCuenta(String nom, String cue, double sal, double tipo) {
 		setNombre(nom);
 		setCuenta(cue);
 		setSaldo(sal);
 	}
 
+	/**
+	 * Método para consultar el estado de la cuenta
+	 * 
+	 * @return El saldo que hay en la cuenta
+	 */
 	public double estado() {
 		return getSaldo();
 	}
 
+	/**
+	 * Método para ingresar dinero
+	 * 
+	 * @param cantidad - Cantidad que se añade al saldo
+	 * @throws Exception Error al ingresar saldo
+	 */
 	public void ingresar(double cantidad) throws Exception {
 		if (cantidad < 0)
 			throw new Exception("No se puede ingresar una cantidad negativa");
 		setSaldo(getSaldo() + cantidad);
 	}
 
+	/**
+	 * Método para retirar dinero
+	 * 
+	 * @param cantidad - Cantidad que se resta al saldo
+	 * @throws Exception - Error al retirar saldo
+	 */
 	public void retirar(double cantidad) throws Exception {
 		if (cantidad <= 0)
 			throw new Exception("No se puede retirar una cantidad negativa");
@@ -33,34 +70,74 @@ public class CCuenta {
 		setSaldo(getSaldo() - cantidad);
 	}
 
+	/**
+	 * Método para obtener el nombre del cliente
+	 * 
+	 * @return El nombre del cliente
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Método para establecer el nombre del cliente
+	 * 
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Método para obtener el número de cuenta
+	 * 
+	 * @return El número de cuenta
+	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
+	/**
+	 * Método para establecer el número de cuenta
+	 * 
+	 * @param cuenta
+	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
+	/**
+	 * Método para obtener el saldo de la cuenta
+	 * 
+	 * @return El saldo que tiene la cuenta
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * Método para establecer el saldo de la cuenta
+	 * 
+	 * @param saldo
+	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * Método para obtener el tipo de interés de la cuenta
+	 * 
+	 * @return El tipo de interés
+	 */
 	public double getTipoInteres() {
 		return tipoInteres;
 	}
 
+	/**
+	 * Método para establecer el tipo de interés de la cuenta
+	 * 
+	 * @param tipoInteres
+	 */
 	public void setTipoInteres(double tipoInteres) {
 		this.tipoInteres = tipoInteres;
 	}
--- deposito/cuentas/Main.java
+++ deposito/cuentas/Main.java
@@ -1,23 +1,44 @@
 package cuentas;
+
+/**
+ * Implementa el inicio del programa.
+ * Clase que gestiona la operativa de cuentas bancarias
+ * 
+ * @author IsmaelMorenoAlfaro
+ * @version 2.0
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		operativa_cuenta(200);
 	}
 
+	
+	/**
+	 * Método que realiza la operativa de una cuenta bancaria
+	 * 
+	 * @param cantidad
+	 */
 	private static void operativa_cuenta(float cantidad) {
+		
 		CCuenta cuenta1;
 		double saldoActual;
 
+		//declarición e inicialización de un objeto de la clase CCuenta
 		cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+		
+		//guarda el saldo actual y lo muestra por pantalla
 		saldoActual = cuenta1.estado();
 		System.out.println("El saldo actual es " + saldoActual);
 
+		//intenta retirar saldo y si ocurre una excepción muestra un mensaje
 		try {
 			cuenta1.retirar(2300);
 		} catch (Exception e) {
 			System.out.print("Fallo al retirar");
 		}
+		
+		//intenta ingresar saldo y si ocurre una excepción muestra un mensaje
 		try {
 			System.out.println("Ingreso en cuenta");
 			cuenta1.ingresar(695);
